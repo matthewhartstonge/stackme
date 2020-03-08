8d0bf2a94179d882b78acc000079a59f3e0e7adc
Matthew Hartstonge <matt@mykro.co.nz>
2020-03-08 22:35:27 +1300

    :arrow_up: wordpress: split out into own folder.

---
+++ wordpress/conform.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Copyright (C) 2016, Mykro Enterprises New Zealand Limited.
+# Copyright (C) 2016, Matthew Hartstonge <matt@mykro.co.nz> and other Authors.
+# All rights reserved.
+#
+# Use of this source code is governed by a BSD 3-clause license that can be found
+# in the LICENSE file.
+
+# Alpine uses non-debian standards for www-data, so for the host we explicitly
+# have to set files to uid/gid 82
+mkdir -p dbdata
+mkdir -p html
+chown -R 82:82 html
+mkdir -p logs
+chown -R 82:82 logs
+chmod 755 html/wp-content
